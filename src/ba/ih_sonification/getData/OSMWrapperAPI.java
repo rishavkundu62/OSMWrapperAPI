9843bf5400073c3292bee22f376f55ad17e30b8d
rishavkundu62 <61747623+rishavkundu62@users.noreply.github.com>
2021-05-17 17:51:31 +0530

    Create OSMWrapperAPI.java

---
+++ src/ba/ih_sonification/getData/OSMWrapperAPI.java
@@ -0,0 +1,388 @@
+package ba.ih_sonification.getData;
+
+/**
+ * (c) Jens K�bler
+ * This software is public domain
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+//import org.osm.lights.diff.OSMNode;
+//import org.osm.lights.upload.BasicAuthenticator;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ */
+public class OSMWrapperAPI {
+
+	private static final String OVERPASS_API = "http://www.overpass-api.de/api/interpreter";
+	private static final String OPENSTREETMAP_API_06 = "http://www.openstreetmap.org/api/0.6/";
+
+//	public static OSMNode getNode(String nodeId) throws IOException, ParserConfigurationException, SAXException {
+//		String string = "http://www.openstreetmap.org/api/0.6/node/" + nodeId;
+//		URL osm = new URL(string);
+//		HttpURLConnection connection = (HttpURLConnection) osm.openConnection();
+//
+//		DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+//		Document document = docBuilder.parse(connection.getInputStream());
+//		HashMap<String, OSMNode> nodes = getNodes(document);
+//		if (!nodes.isEmpty()) {
+//			return nodes.iterator().next();
+//		}
+//		return null;
+//	}
+
+	/**
+	 * 
+	 * @param lat the latitude
+	 * @param lon the longitude
+	 * @param vicinityRange bounding box in this range
+	 * @return the xml document containing the queries nodes
+	 * @throws IOException
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 */
+	@SuppressWarnings("nls")
+	private static Document getXML(double lat, double lon, double vicinityRange) throws IOException, SAXException,
+			ParserConfigurationException {
+
+		DecimalFormat format = new DecimalFormat("##0.0000000", DecimalFormatSymbols.getInstance(Locale.ENGLISH)); //$NON-NLS-1$
+		String left = format.format(lon - vicinityRange);
+		String bottom = format.format(lat - vicinityRange);
+		String right = format.format(lon + vicinityRange);
+		String top = format.format(lat + vicinityRange);
+
+		String string = OPENSTREETMAP_API_06 + "map?bbox=" + left + "," + bottom + "," + right + ","
+				+ top;
+		URL osm = new URL(string);
+		HttpURLConnection connection = (HttpURLConnection) osm.openConnection();
+
+		DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+		Document tempDoc = docBuilder.parse(connection.getInputStream());
+		
+//		System.out.print(tempDoc.toString());
+		return tempDoc;
+	}
+
+	public static Document getXMLFile(String location) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+		return docBuilder.parse(location);
+	}
+	
+	/**
+	 * 
+	 * @param xmlDocument 
+	 * @return a list of ways, representing a building or a part of a building
+	 */
+	@SuppressWarnings("nls")
+	public static HashMap<String, OSMWay> getMultipolygons(Document xmlDocument) {
+		
+		// Erstelle leere Liste von OSMWays
+		HashMap<String, OSMWay> osmWays = new HashMap<String, OSMWay>();
+
+		// Document xml = getXML(8.32, 49.001);
+		Node osmRoot = xmlDocument.getFirstChild();
+		// Zugriff auf alle Nodes mit einem "Tab" oder einer "Einr�ckung"
+		NodeList osmXMLNodes = osmRoot.getChildNodes();
+		
+		// Checke, ob jedes Element ...
+		for (int i = 1; i < osmXMLNodes.getLength(); i++) {
+			
+			Node item = osmXMLNodes.item(i);
+			// ... eine Way ist
+			if (item.getNodeName().equals("way")) {
+				
+				boolean hasBuildingTag = false;
+				
+				NodeList ndOrTagXMLNodes = item.getChildNodes();
+				
+				ArrayList<String> refNodesIDs = new ArrayList<String>();
+				Map<String, String> tags = new HashMap<String, String>();
+				
+				for (int j = 1; j < ndOrTagXMLNodes.getLength(); j++) {
+					
+					Node ndOrTagItem = ndOrTagXMLNodes.item(j);
+					
+					NamedNodeMap ndOrTagAttributes = ndOrTagItem.getAttributes();
+					if (ndOrTagAttributes != null) {
+						if (ndOrTagItem.getNodeName().equals("tag")) {
+							tags.put(ndOrTagAttributes.getNamedItem("k").getNodeValue(), ndOrTagAttributes.getNamedItem("v")
+								.getNodeValue());
+							
+							// Checke, ob es sich um eine Way handelt, die was mit buildings zu tun hat.
+							if (ndOrTagAttributes.getNamedItem("k").getNodeValue().startsWith("building")) {
+								hasBuildingTag = true;
+							}
+							
+						} else if (ndOrTagItem.getNodeName().equals("nd")) {
+							refNodesIDs.add(ndOrTagAttributes.getNamedItem("ref").getNodeValue());
+						}
+						
+					}
+				}
+				
+				NamedNodeMap attributes = item.getAttributes(); // Attribute rechts von way
+				Node namedItemID = attributes.getNamedItem("id");
+				Node namedItemVersion = attributes.getNamedItem("version");
+
+				String id = namedItemID.getNodeValue();
+				String version = "0";
+				if (namedItemVersion != null) {
+					version = namedItemVersion.getNodeValue();
+				}
+
+				if (hasBuildingTag) {
+					osmWays.put(id, new OSMWay(id, refNodesIDs, tags, version));
+				}
+			}
+
+		}
+		return osmWays;
+	}
+
+	/**
+	 * 
+	 * @param xmlDocument 
+	 * @return a list of openseamap nodes extracted from xml
+	 */
+	@SuppressWarnings("nls")
+	public static HashMap<String, OSMNode> getNodes(Document xmlDocument) {
+		
+		// Erstelle leere Liste von OSMNodes
+		HashMap<String, OSMNode> osmNodes = new HashMap<String, OSMNode>();
+
+		// Document xml = getXML(8.32, 49.001);
+		Node osmRoot = xmlDocument.getFirstChild();
+		// Zugriff auf alle Nodes mit einem "Tab" oder einer "Einrückung"
+		NodeList osmXMLNodes = osmRoot.getChildNodes();
+		
+		// Checke, ob jedes Element ...
+		for (int i = 1; i < osmXMLNodes.getLength(); i++) {
+			
+			Node item = osmXMLNodes.item(i);
+			// ... eine Node ist
+			if (item.getNodeName().equals("node")) {
+				
+				// Tags speichern == Inhalt der ChildNodes speichern
+				NodeList tagXMLNodes = item.getChildNodes();
+				Map<String, String> tags = new HashMap<String, String>();
+				for (int j = 1; j < tagXMLNodes.getLength(); j++) {
+					Node tagItem = tagXMLNodes.item(j);
+					NamedNodeMap tagAttributes = tagItem.getAttributes();
+					if (tagAttributes != null) {
+						tags.put(tagAttributes.getNamedItem("k").getNodeValue(), tagAttributes.getNamedItem("v")
+								.getNodeValue());
+					}
+				}
+				
+				NamedNodeMap attributes = item.getAttributes();
+				// Wichtige Attribute speichern
+				Node namedItemID = attributes.getNamedItem("id");
+				Node namedItemLat = attributes.getNamedItem("lat");
+				Node namedItemLon = attributes.getNamedItem("lon");
+				Node namedItemVersion = attributes.getNamedItem("version");
+
+				// Vorbereitung, um eine OSMNode mit den benötigten Informationen zu erstellen
+				String id = namedItemID.getNodeValue();
+				String latitude = namedItemLat.getNodeValue();
+				String longitude = namedItemLon.getNodeValue();
+				String version = "0";
+				if (namedItemVersion != null) {
+					version = namedItemVersion.getNodeValue();
+				}
+				
+				// Erstelle eine OSMNode
+				osmNodes.put(id, new OSMNode(id, latitude, longitude, version, tags));
+			}
+
+		}
+		return osmNodes;
+	}
+
+	public static HashMap<String, OSMNode> getOSMNodesInVicinity(double lat, double lon, double vicinityRange) throws IOException,
+			SAXException, ParserConfigurationException { 
+		return OSMWrapperAPI.getNodes(getXML(lat, lon, vicinityRange)); // lon and lat were swapped
+	}
+
+	/**
+	 * 
+	 * @param query the overpass query
+	 * @return the nodes in the formulated query
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 */
+	public static Document getNodesViaOverpass(String query) throws IOException, ParserConfigurationException, SAXException {
+		String hostname = OVERPASS_API;
+		String queryString = readFileAsString(query);
+
+		URL osm = new URL(hostname);
+		HttpURLConnection connection = (HttpURLConnection) osm.openConnection();
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+		DataOutputStream printout = new DataOutputStream(connection.getOutputStream());
+		printout.writeBytes("data=" + URLEncoder.encode(queryString, "utf-8"));
+		printout.flush();
+		printout.close();
+
+		DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+		return (testDoc = docBuilder.parse(connection.getInputStream()));
+	}
+	
+	public static Document testDoc;
+
+	/**
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws java.io.IOException
+	 */
+	private static String readFileAsString(String filePath) throws java.io.IOException {
+		StringBuffer fileData = new StringBuffer(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		char[] buf = new char[1024];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+		reader.close();
+		return fileData.toString();
+	}
+	
+	private static void printNodesHeading() {
+		System.out.println("----------------------");
+		System.out.println("---------Nodes--------");
+		System.out.println("----------------------");
+	}
+	
+	private static void printWaysHeading() {
+		System.out.println("----------------------");
+		System.out.println("---------Ways---------");
+		System.out.println("----------------------");
+	}
+	
+	private static HashMap<String, OSMNode> osmNodesInVicinity;
+	
+	private static void printNodes(double lat, double lon, double radius)
+			throws IOException, SAXException, ParserConfigurationException {
+		printNodesHeading();
+		
+		osmNodesInVicinity = getOSMNodesInVicinity(lat, lon, radius);
+		
+		for (String osmNodeID : osmNodesInVicinity.keySet()) {
+			System.out.println(osmNodesInVicinity.get(osmNodeID));
+		}
+	}
+	
+	private static HashMap<String, OSMWay> osmWaysInVicinity;
+
+	private static void printWays(double lat, double lon, double radius)
+			throws IOException, SAXException, ParserConfigurationException {
+		printWaysHeading();
+		
+		osmWaysInVicinity = OSMWrapperAPI.getMultipolygons(getXML(lat, lon, radius));
+		
+		for (String osmWayID : osmWaysInVicinity.keySet()) {
+			System.out.println(osmWaysInVicinity.get(osmWayID));
+		}
+	}
+
+	/**
+	 * main method that simply reads some nodes
+	 * 
+	 * @param args
+	 * @throws IOException
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 */
+	public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+		
+		//49.01105,8.4147
+		// Gebäude links vom Architekturgebäude: 49.0118, 8.41
+		// id des Gebäude-Multipolygons: 23711721
+		// linker Punkt des Gebäudes: 256759672
+		double lat = 49.0118;
+		double lon = 8.41;
+		double radius = 0.0007;
+		
+		Authenticator.setDefault(new BasicAuthenticator("youruser", "yourpassword"));
+		
+		printWays(lat, lon, radius);
+		
+		printNodes(lat, lon, radius);
+		
+		int counter = 0;
+		
+		for (String OSMWayID: osmWaysInVicinity.keySet()) {
+			
+			OSMWay currentWay = osmWaysInVicinity.get(OSMWayID);
+			
+			// Lege eine Leere Nodes-Liste an für die aktuell betrachtete Way ...
+			LinkedHashMap<String, OSMNode> refNodes = new LinkedHashMap<String, OSMNode>();
+			
+			ArrayList<String> refNodesIDs = currentWay.getRefNodesIDs();
+			
+			// Für jede NodeID, suche die dazugehörige Node und füge sie in die LinkedHashMap ein
+			for (String OSMNodeID: refNodesIDs) {
+				
+				OSMNode currentNode = osmNodesInVicinity.get(OSMNodeID);
+				
+				refNodes.put(OSMNodeID, currentNode);
+				
+				counter++;
+			}
+			
+			currentWay.setRefNodes(refNodes);
+			
+			
+		}
+		
+		System.out.println("\nAnzahl Ways = " + osmWaysInVicinity.size() + ",\n"
+				+ "Anzahl Way-Nodes (Anfangsnodes doppelt) = " + counter + ", Anzahl Nodes = " + osmNodesInVicinity.size());
+
+	}
+	
+}
